2d02e634bec75657fb6710c54d6b8b8facfeaa4b
Michel <101409380+Miro8D@users.noreply.github.com>
2025-08-02 19:06:39 +0300

    Update login-page.jsx

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
         setError('');
         
         try {
-            const response = await fetch('http://localhost:8000/api/accounts/login', {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/accounts/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
