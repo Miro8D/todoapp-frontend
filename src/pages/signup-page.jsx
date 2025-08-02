ef58efeae189c51cf7a8c611ad429b4ecda01dc7
Michel <101409380+Miro8D@users.noreply.github.com>
2025-08-02 19:32:02 +0300

    Update signup-page.jsx

--- src/pages/signup-page.jsx
+++ src/pages/signup-page.jsx
@@ -18,7 +18,7 @@ export default function SignupPage() {
         setError('');
         
         try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/accounts/signup`, {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/accounts/signup`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
